3684f8dce34f2d135f1c72bc41f614f961aedaf2
Jennifer Hogueison <fox@fenny.space>
2018-02-18 22:04:04 -0800

    asdf

---
+++ Udacity - Front-end Web Dev - GWG Scholarship/Unit 4 - Javascript Syntax/bill-and-tip.js
@@ -0,0 +1,9 @@
+/*
+ * Programming Quiz: What's my Name? (2-9)
+ */
+
+// your code goes here
+var bill = 10.25 + 3.99 + 7.15;
+var tip = 0.15 * bill;
+var total = bill + tip;
+console.log(total);
---
+++ Udacity - Front-end Web Dev - GWG Scholarship/Unit 4 - Javascript Syntax/message.js
@@ -0,0 +1,26 @@
+/*
+ * Programming Quiz: One Awesome Message (2-12)
+ *
+ * 1. Create the variables:
+ *     - firstName
+ *     - interest
+ *     - hobby
+ * 2. Create a variable named awesomeMessage and, using string concatenation
+      and the variables above, create an awesome message.
+ * 3. Print the awesomeMessage variable to the console.
+ */
+
+/*
+ * Notes:
+ * - Using the above as an example, firstName would have been assigned to
+ *   "Julia", interest to "cats", and hobby to "play video games".
+ * - Be sure to include spaces and periods where necessary!
+ */
+
+// Add your code here
+
+var firstName = "fox";
+var interest = "fox";
+var hobby = "fox";
+var awesomeMessage = firstName + interest + hobby;
+console.log(awesomeMessage);
